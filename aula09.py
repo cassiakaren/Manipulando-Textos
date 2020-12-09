57db633e26f73182c68954e7c63a28508feb5481
cassiakaren <71670140+cassiakaren@users.noreply.github.com>
2020-12-09 19:32:26 -0300

    comeco da aula 09

---
+++ aula09.py
@@ -0,0 +1,23 @@
+#FATIAMENTO
+'''frase='Curso em Video Python'
+print(frase[3])#vai printar a quarta letra
+frase='Curso em Video Python'
+print(frase[:13])#vai de um caracter a outro
+frase='Curso em Video Python'
+print(frase[0:15:2])#vai de um caracter a outro pulando de 2 em 2
+frase='Curso em Video Python'
+print(frase[::2])
+frase='Curso em Video Python'
+print(frase.upper().count('O'))#conta quantas vezes tem 'o' e deixa tudo maiuscul
+frase='    Curso em Video Python   '
+print(len(frase.strip()))#ler e imprime o tamanho da frase'''
+'''frase='Curso em Video Python'
+print(frase.split())#cria uma lista para cada caracter
+#NA PRÁTICA
+frase='Curso em Video Python'
+dividido=frase.split()
+print(dividido[0])#dividido é uma lista 0= é o primeiro item da lista'''
+frase='Curso em Video Python'
+dividido=frase.split()
+print(dividido[2][3])#um elemento dentro de uma lisgta especifica
+
---
+++ ex22.py
@@ -0,0 +1,14 @@
+'''Crie um programa que leia o nome completo de uma pessoa e mostre:
+- O nome com todas as letras maiusculas 
+- O nome com todas as letras maiusculas
+- Quantas letras ao todo sem considerar espaços
+- Quantas letras tem o primeiro nome'''
+frase=str(input('Digite seu nome: '))
+f1=frase.upper()
+f2=frase.lower()
+'''f3=frase.count().strip()
+f4=frase.join().count([0])'''
+print(f1)
+print(f2)
+'''print(f3)
+print(f4)'''
