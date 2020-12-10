06cd66441a7fcfbf8abcd1c66124151e8dde6be8
cassiakaren <71670140+cassiakaren@users.noreply.github.com>
2020-12-10 16:54:51 -0300

    ex23

---
+++ ex23.py
@@ -0,0 +1,11 @@
+'''Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.'''
+num=int(input('Digite um numero: '))
+u=num // 1 % 10
+d=num // 10 % 10
+c=num // 100 % 10
+m=num // 1000 % 10
+print('Analizando o numero {}'.format(num))
+print('unidade:{}'.format(u))
+print('dezena:{}'.format(d))
+print('centena:{}'.format(c))
+print('milhar:{}'.format(m))
