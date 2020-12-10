882480f386242de2b80f1e2cfb8c2df920d80281
cassiakaren <71670140+cassiakaren@users.noreply.github.com>
2020-12-09 21:11:34 -0300

    correçao ex21

--- ex22.py
+++ ex22.py
@@ -3,12 +3,11 @@
 - O nome com todas as letras maiusculas
 - Quantas letras ao todo sem considerar espaços
 - Quantas letras tem o primeiro nome'''
-frase=str(input('Digite seu nome: '))
-f1=frase.upper()
-f2=frase.lower()
-'''f3=frase.count().strip()
-f4=frase.join().count([0])'''
-print(f1)
-print(f2)
-'''print(f3)
-print(f4)'''
+nome=str(input('Digite seu nome completo: ')).strip()
+print('Analizando seu nome...')
+print('Seu nome em letra maiuscula é: {}'.format(nome.upper()))
+print('Seu nome em letra minuscula é: {}'.format(nome.lower()))
+print('Seu nome tem um total de {} caracteres'.format(len(nome)-nome.count(' ')))
+#print('seu primeiro nome tem {} letras'.format(nome.find(' ')))
+separa=nome.split()
+print('seu primeiro nome é {} e ele tem {} letras'.format(separa[0],len(separa[0])))
