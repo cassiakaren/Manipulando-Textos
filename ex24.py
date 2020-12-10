4abdd324b721e0bfdc95eba83bdd11545a5595bb
cassiakaren <71670140+cassiakaren@users.noreply.github.com>
2020-12-10 17:08:39 -0300

    ex24
    'Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”

---
+++ ex24.py
@@ -0,0 +1,3 @@
+'''Crie um programa que leia o nome de uma cidade diga se ela começa ou não com o nome “SANTO”.'''
+cidade=str(input('Digite o nome da sua cidade: ')).strip()
+print(cidade[0:5].upper()=='SANTO')
